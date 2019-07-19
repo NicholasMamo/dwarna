9762ab92be4d01cf7b901aa6f07b753e800d5c9d
Nicholas Mamo <nicholasmamo@gmail.com>
2019-07-19 12:33:50 +0200

    Create new card import workflow structure

--- biobank-plugin/public/js/hyperledger/card.js
+++ biobank-plugin/public/js/hyperledger/card.js
@@ -52,6 +52,46 @@ jQuery(document).ready(function(){
 });
 
 /**
+ * Get the user's card for the study.
+ * This card allows them to make requests to the Hyperledger API for this particular study.
+ *
+ * @param {object}	element - The study DOM element.
+ * @param {int}		study_id - The study ID that is being loaded.
+ */
+function getCard(element, study_id) {
+	if (jQuery(element).attr('aria-expanded') == 'false') {
+		/*
+		 * Whenever a study is opened, get the card.
+		 * Otherwise, do nothing.
+		 */
+		console.log(`Getting card for study ${study_id}`);
+
+		jQuery.get(`${ajax_base_path}get_username.php`).then(function(response) {
+			/*
+			* First get the user's username.
+			* TODO: Check the user role as well.
+			*
+			* If the user is logged in, check whether they have a temporary card for this study.
+			* If they do not, a new card is created instead.
+			*/
+			if (response) {
+				username = response;
+				loadCard();
+			} else {
+				/*
+				 * If the user is not logged in, check if they have an access token.
+				 * If they do, clear that token and refresh the page for the change to take effect.
+				 */
+				if (getCookie(hyperledger_access_token)) {
+					setCookie(hyperledger_access_token, "", 0);
+					location.reload();
+				}
+			}
+		});
+	}
+}
+
+/**
  * Load the user's card if they have one.
  * This card allows them to make requests to the Hyperledger API.
  */
