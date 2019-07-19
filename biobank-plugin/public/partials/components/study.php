4e73df296aaf6a6952c9dfd529d7f91c92ec99f1
Nicholas Mamo <nicholasmamo@gmail.com>
2019-07-19 11:33:26 +0200

    Update the study to display each study in a form

--- biobank-plugin/public/partials/components/study.php
+++ biobank-plugin/public/partials/components/study.php
@@ -7,8 +7,15 @@
 			<label for="<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>">Participate</label>
 		</th>
 		<td>
-			<input name='<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type='hidden' value='0'>
-			<input id = '<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>' name = '<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type = 'checkbox' <?= $consent === True ? "checked" : "" ?>>
+			<form class="<?= $this->plugin_name ?>-form" id="consent_form" method="post" name="consent_form" action=<?php echo esc_url(admin_url("admin-post.php")); ?>>
+				<input type="hidden" name="action" value="consent_form">
+				<?php wp_nonce_field("consent_form", "consent_nonce"); ?>
+
+				<input name='<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type='hidden' value='0'>
+				<input id = '<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>' name = '<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type = 'checkbox' <?= $consent === True ? "checked" : "" ?>>
+
+				<input type = "submit" class = "primary" />
+			</form>
 		</td>
 	</tr>
 </table>
