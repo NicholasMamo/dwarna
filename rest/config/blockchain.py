895fc75f9805aee8f958e1b806b3d3f4f2753e50
Nicholas Mamo <nicholasmamo@gmail.com>
2019-07-19 11:18:37 +0200

    Create new configuration option to enable multi-card workflow

--- rest/config/blockchain.py
+++ rest/config/blockchain.py
@@ -15,3 +15,12 @@ multiuser_port = 3000
 :var multiuser_port: The port where the Hyperledger Composer REST API listens to multi-user requests.
 :vartype multiuser_port: int
 """
+
+multi_card = False
+"""
+:var multi_card: A flag that determines whether a multi-card workflow is followed.
+				 If set to true, then each participant will have one identity for each study that they ever participate in.
+				 Otherwise, a new identity is generated the first time they participate in a study.
+				 Later, the same identity would be used over and over again.
+:vartype multi_card: bool
+"""
