f8b989a3e6b713cac7fec563448e145e658a2394
Nicholas Mamo <nicholasmamo@gmail.com>
2019-07-19 11:28:18 +0200

    Clean up study code - remove the commented attribute display

--- biobank-plugin/public/partials/components/study.php
+++ biobank-plugin/public/partials/components/study.php
@@ -2,19 +2,6 @@
 <p class="biobank-description"><?= $study->study->description ?></p>
 <p class="biobank-homepage"><a href="<?= $study->study->homepage ?>" target="_blank">Read more</a></p>
 <table class="form-table">
-<!-- <?php
-foreach ($study->attributes as $attribute) {
-	$field = \fields\create_form_field($this->plugin_name, $study->study->study_id, $attribute, $defaults);
-?>
-	<tr class="form-field form-required">
-		<th scope="row">
-			<label for="<?= $attribute->attribute_id ?>"><?= $attribute->name ?></label>
-		</th>
-		<td>
-			<?= $field ?>
-		</td>
-	</tr>
-<?php } ?> -->
 	<tr class="form-field form-required">
 		<th scope="row">
 			<label for="<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>">Participate</label>
