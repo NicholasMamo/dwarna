d158a5c4de8741751b0c8a74f5fd4529c1b0c175
Nicholas Mamo <nicholasmamo@gmail.com>
2019-07-19 11:58:58 +0200

    Make the studies dropdown elements

--- biobank-plugin/public/partials/components/study.php
+++ biobank-plugin/public/partials/components/study.php
@@ -1,23 +1,30 @@
-<h4><?= $study->study->name ?></h4>
-<p class="biobank-description"><?= $study->study->description ?></p>
-<p class="biobank-homepage"><a href="<?= $study->study->homepage ?>" target="_blank">Read more</a></p>
-<form class="<?= $this->plugin_name ?>-form" id="consent_form" method="post" name="consent_form" action=<?php echo esc_url(admin_url("admin-post.php")); ?>>
+<h4><a href='#study-<?= $study->study->study_id ?>' data-toggle="collapse"
+	   role="button" aria-expanded="false" aria-controls="collapseExample">
+	   <?= $study->study->name ?>
+    </a></h4>
+<form class="<?= $this->plugin_name ?>-form" id="consent-form-<?= $study->study->study_id ?>"
+	  method="post" name="consent_form" action=<?php echo esc_url(admin_url("admin-post.php")); ?>>
 	<input type="hidden" name="action" value="consent_form">
 	<?php wp_nonce_field("consent_form", "consent_nonce"); ?>
 
-	<div class='row'>
-		<div class='col-md-5 text-md-right'>
-			<label for="<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>">Participate</label>
-		</div>
-		<div class='col-md-7'>
-			<input name='<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type='hidden' value='0'>
-			<input id = '<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>' name = '<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type = 'checkbox' <?= $consent === True ? "checked" : "" ?>>
-		</div>
-	</div>
 
-	<div class='row'>
-		<div class='col-md-7 offset-md-5'>
-			<input type = "submit" class = "primary" />
+	<div id='study-<?= $study->study->study_id ?>' class='collapse'>
+		<p class="biobank-description"><?= $study->study->description ?></p>
+		<p class="biobank-homepage"><a href="<?= $study->study->homepage ?>" target="_blank">Read more</a></p>
+		<div class='row'>
+			<div class='col-md-5 text-md-right'>
+				<label for="<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>">Participate</label>
+			</div>
+			<div class='col-md-7 collapse'>
+				<input name='<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type='hidden' value='0'>
+				<input id = '<?= $this->plugin_name ?>-study-<?= $study->study->study_id ?>' name = '<?= $this->plugin_name ?>[study][<?= $study->study->study_id ?>][consent]' type = 'checkbox' <?= $consent === True ? "checked" : "" ?>>
+			</div>
+		</div>
+
+		<div class='row'>
+			<div class='col-md-7 offset-md-5'>
+				<input type = "submit" class = "btn btn-primary float-left" />
+			</div>
 		</div>
 	</div>
 </form>
