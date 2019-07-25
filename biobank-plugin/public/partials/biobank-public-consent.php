319d792e6fc6f8c0eb4b14b8ccd3caec0618cc67
Nicholas Mamo <nicholasmamo@gmail.com>
2019-07-25 13:02:50 +0200

    Update the consent page to show a list of studies

--- biobank-plugin/public/partials/biobank-public-consent.php
+++ biobank-plugin/public/partials/biobank-public-consent.php
@@ -40,7 +40,18 @@ $refresh = (isset($_GET["return"]) && $_GET["return"] == "update_consent");
 	}
 	?>
 
-	<?php foreach ($active_studies->data as $study) {
-		include(plugin_dir_path(__FILE__) . "components/study.php");
-	} ?>
+	<form id='study-form' method="post" name="consent_form"
+		  action=<?php echo esc_url(admin_url("admin-post.php")); ?>>
+		<input type="hidden" name="action" value="study_form">
+		<?php wp_nonce_field("consent_form", "consent_nonce"); ?>
+		<input id='<?= $this->plugin_name ?>-study'
+			   name='<?= $this->plugin_name ?>[study][study_id]'
+			   type='hidden' value=''>
+	</form>
+
+	<ul>
+	<?php foreach ($active_studies->data as $study) {?>
+		<li><a href='#' onclick='getCard(this, <?= $study->study->study_id ?>); return false;'><?= $study->study->name ?></a></li>
+	<?php } ?>
+	</ul>
 </div>
