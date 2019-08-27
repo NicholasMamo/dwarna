0fb0608bbd665682cf42b005c26983ad0862f409
Nicholas Mamo <nicholasmamo@gmail.com>
2019-08-27 15:01:13 +0200

    Create a new card mode test class

---
+++ rest/tests/test_card_modes.py
@@ -0,0 +1,38 @@
+"""
+Test the different card modes of the biobank REST API.
+"""
+
+import os
+import sys
+import time
+
+path = sys.path[0]
+path = os.path.join(path, "../")
+if path not in sys.path:
+	sys.path.insert(1, path)
+
+import main
+
+from .environment import *
+
+from .test import BiobankTestCase, rest_context
+
+class CardModeTest(BiobankTestCase):
+	"""
+	An abstract class that describes the tests that should be created by card mode tests.
+	"""
+
+	@classmethod
+	@BiobankTestCase.isolated_test
+	def setUpClass(self, single_card):
+		"""
+		Connect with the database, create the schema and start the server.
+
+		:param single_card: A boolean indicating whether the REST API should be started in single-card mode.
+							If false, the REST API is started in multi-card mode.
+		:type single_card: bool
+		"""
+
+		create_testing_environment()
+		main.main(TEST_DATABASE, TEST_OAUTH_DATABASE, PORT, single_card=single_card)
+		time.sleep(1) # wait so as not to overload the server with requests
