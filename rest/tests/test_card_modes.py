cbae65d1c3d894cae51d1dbb926b3d0bb0ecee9a
Nicholas Mamo <nicholasmamo@gmail.com>
2019-08-27 15:46:28 +0200

    Createa a seed function for the card mode tests

--- rest/tests/test_card_modes.py
+++ rest/tests/test_card_modes.py
@@ -6,6 +6,8 @@ import os
 import sys
 import time
 
+from abc import ABC, abstractmethod
+
 path = sys.path[0]
 path = os.path.join(path, "../")
 if path not in sys.path:
@@ -20,11 +22,16 @@ from .test import BiobankTestCase, rest_context
 class CardModeTest(BiobankTestCase):
 	"""
 	An abstract class that describes the tests that should be created by card mode tests.
+
+	:cvar _study_ids: A list of study IDs that are used in the test cases.
+	:vartype _study_ids: list of str
 	"""
+
+	_study_ids = []
 
 	@classmethod
 	@BiobankTestCase.isolated_test
-	def setUpClass(self, single_card):
+	def setUpClass(self, single_card=True):
 		"""
 		Connect with the database, create the schema and start the server.
 
@@ -36,6 +43,63 @@ class CardModeTest(BiobankTestCase):
 		create_testing_environment()
 		main.main(TEST_DATABASE, TEST_OAUTH_DATABASE, PORT, single_card=single_card)
 		time.sleep(1) # wait so as not to overload the server with requests
+		CardModeTest.seed()
+
+	@classmethod
+	def seed(self):
+		"""
+		Seed the database before tests.
+		"""
+		self = BiobankTestCase()
+		token = self._get_access_token(
+		["change_card", "create_study", "view_study", "create_participant"])["access_token"]
+
+		CardModeTest._study_ids = [
+		self._generate_study_name(),
+		self._generate_study_name(),
+		self._generate_study_name(),
+		self._generate_study_name(),
+		]
+
+		"""
+		Create the initial data.
+		"""
+
+		response = self.send_request("POST", "study", {
+		"study_id": CardModeTest._study_ids[0],
+		"name": "ALS",
+		"description": "ALS Study",
+		"homepage": "http://um.edu.mt",
+		}, token)
+		self.assertEqual(response.status_code, 200)
+
+		response = self.send_request("POST", "study", {
+		"study_id": CardModeTest._study_ids[1],
+		"name": "Diabetes",
+		"description": "Diabetes study",
+		"homepage": "http://um.edu.mt",
+		}, token)
+		self.assertEqual(response.status_code, 200)
+
+		"""
+		Ensure that the data was created.
+		"""
+
+		response = self.send_volatile_request("GET", "study", { "study_id": CardModeTest._study_ids[0] }, token)
+		response = self.send_volatile_request("GET", "study", { "study_id": CardModeTest._study_ids[1] }, token)
+
+		"""
+		Create a participant - `p2323`.
+		"""
+
+		for participant in [2323]:
+			response = self.send_request("POST", "participant", {
+			"username": f"p{participant}",
+			"name": participant,
+			"email": f"participant@test.com"
+			}, token)
+			self.assertEqual(response.status_code, 200)
+
 
 class SingleCardModeTest(CardModeTest):
 	"""
