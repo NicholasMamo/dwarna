b58ba0d3f3b150c0d07f97c416fcb70126658de4
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-14 16:13:58 +0200

    Update the setup testing environment to use the new connection

--- setup/tests/environment.py
+++ setup/tests/environment.py
@@ -6,10 +6,15 @@ from os.path import expanduser
 import psycopg2
 
 path = sys.path[0]
-path = os.path.join(path, "../")
-if path not in sys.path:
-	sys.path.insert(1, path)
+setup_path = os.path.join(path, "..")
+if setup_path not in sys.path:
+	sys.path.insert(1, setup_path)
 
+rest_path = os.path.join(path, '..', '..', 'rest')
+if rest_path not in sys.path:
+	sys.path.insert(1, rest_path)
+
+from connection.db_connection import PostgreSQLConnection
 import minimal_schema
 
 TEST_DATABASE = "biobank_test"
@@ -17,52 +22,32 @@ TEST_DATABASE = "biobank_test"
 The database that is used as a testing environment.
 """
 
-cursor, con = None, None
-
 def create_testing_environment():
 	"""
 	Create a testing environment so that the actual database is not altered.
 	"""
 
-	con = get_connection("postgres")
-	con.autocommit = True # commit all changes automatically
-	cursor = con.cursor() # fetch the cursor
+	connection = PostgreSQLConnection.connect('postgres')
 
 	"""
 	Ensure that the database does not already exist.
 	Create it only if it doesn't exist.
 	"""
-	cursor.execute("""SELECT 1 FROM pg_database WHERE datname = '%s'""" % TEST_DATABASE)
-	print("Database has to be created" if not cursor.rowcount else "Database already exists")
-	if (cursor.rowcount == 0):
-		cursor.execute("CREATE DATABASE %s" % TEST_DATABASE)
+	exists = connection.exists("""SELECT 1 FROM pg_database WHERE datname = '%s'""" % TEST_DATABASE)
+	print("Database has to be created" if not exists else "Database already exists")
+	if (not exists):
+		connection.execute("CREATE DATABASE %s" % TEST_DATABASE)
 	minimal_schema.create_schema(TEST_DATABASE)
-
-	cursor.close()
-	con.close()
-
-def get_connection(database=TEST_DATABASE):
-	# get the connection details from the .pgpass file
-	home = expanduser("~")
-	with open(os.path.join(home, ".pgpass"), "r") as f:
-		host, port, _, username, password = f.readline().strip().split(":")
-		con = psycopg2.connect(dbname=database, user=username, host=host, password=password)
-	return con
 
 def clear():
 	"""
 	Clear all the data from the database.
 	"""
 
-	con = get_connection()
-	cursor = con.cursor() # fetch the cursor
+	connection = PostgreSQLConnection.connect(TEST_DATABASE)
 
-	cursor.execute("DELETE FROM users")
-	cursor.execute("DELETE FROM studies")
-	con.commit()
-
-	cursor.close()
-	con.close()
+	connection.execute("DELETE FROM users")
+	connection.execute("DELETE FROM studies")
 
 if __name__ == "__main__":
 	create_testing_environment()
