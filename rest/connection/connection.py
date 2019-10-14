4185da7e39fee81d1b6dfe62b722e7eb7d4f32f5
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-14 12:45:22 +0200

    Update the abstract database connection class to require a connect method

--- rest/connection/connection.py
+++ rest/connection/connection.py
@@ -2,11 +2,24 @@
 The connection to the PostgreSQL, which also executes commands on behalf of applications.
 """
 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractstaticmethod
 
 class Connection(ABC):
 	"""
 	A generic connection class
 	"""
 
-	pass
+	@abstractstaticmethod
+	def connect(database, *args, **kwargs):
+		"""
+		Connect to the database having the given name.
+		The function should return the connection object wrapped in the class.
+
+		:param database: The name of the database to connect to.
+		:type database: str
+
+		:return: The database connection.
+		:rtype: :class:`connection.db_connection.Connection`
+		"""
+
+		pass
