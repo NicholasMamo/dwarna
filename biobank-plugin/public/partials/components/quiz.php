6ad411efac9a105f89df7a0f11134640db24a848
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-15 16:32:04 +0200

    Create the initial quiz view structure

---
+++ biobank-plugin/public/partials/components/quiz.php
@@ -0,0 +1,18 @@
+<div id ='quiz'>
+	<?php foreach ($quiz as $i => $question) { ?>
+	<div class='question'>
+		<div class='row mx-1'><?= $question->question ?></div>
+		<div class='row explanation'><?= $question->explanation ?></div>
+		<div class='row mx-3 mb-2'>
+			<div class='col-md-6 text-center'>
+				<input id="biobank-question-<?= $i ?>-yes" class='radio' value='yes' type='radio'>
+				<label for="biobank-question-<?= $i ?>-yes">Yes</label>
+			</div>
+			<div class='col-md-6 text-center'>
+				<input id="biobank-question-<?= $i ?>-no" class='radio' value='no' type='radio'>
+				<label for="biobank-question-<?= $i ?>-no">No</label>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+</div>
