091a1f65d28fcf444e075ad18fcf15987c66c2b3
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-15 14:24:38 +0200

    Update the study page to make the trail area take less space

--- biobank-plugin/public/partials/components/study.php
+++ biobank-plugin/public/partials/components/study.php
@@ -14,28 +14,30 @@ if (isset($error) && ! empty($error)) {
 }
 ?>
 
-<ul>
-<?php
-foreach ($timeline as $timestamp => $changes) {
-?>
-	<li><?= DateTime::createFromFormat("U", $timestamp)->format("jS M Y \a\\t H:i") ?>
-		<ul>
+<div id='biobank-consent-trail'>
+	<ul>
 	<?php
-		$changes = (array) $changes;
-		foreach ($changes as $study_id => $consent) {
+	foreach ($timeline as $timestamp => $changes) {
 	?>
-			<?php if ($study_id == $study->study->study_id) { ?>
-			<li>
-				<?= $consent == 1 ? "Give to" : "Withdraw from" ?> <?= $studies->data->$study_id->study->name ?>
-			</li>
-			<?php } ?>
-	<?php
-		}
-	?>
-		</ul>
-	</li>
-<?php } ?>
-</ul>
+		<li><?= DateTime::createFromFormat("U", $timestamp)->format("jS M Y \a\\t H:i") ?>
+			<ul>
+		<?php
+			$changes = (array) $changes;
+			foreach ($changes as $study_id => $consent) {
+		?>
+				<?php if ($study_id == $study->study->study_id) { ?>
+				<li>
+					<?= $consent == 1 ? "Give to" : "Withdraw from" ?> <?= $studies->data->$study_id->study->name ?>
+				</li>
+				<?php } ?>
+		<?php
+			}
+		?>
+			</ul>
+		</li>
+	<?php } ?>
+	</ul>
+</div>
 
 <form class="<?= $this->plugin_name ?>-form" id="consent-form-<?= $study->study->study_id ?>"
 	  method="post" name="consent_form" action=<?php echo esc_url(admin_url("admin-post.php")); ?>>
