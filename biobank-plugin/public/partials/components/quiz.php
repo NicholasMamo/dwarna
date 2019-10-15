c4996637d0f89b7b98ddebbf2907f2b26f19b692
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-15 16:49:32 +0200

    Add the question's answer to each quiz question

--- biobank-plugin/public/partials/components/quiz.php
+++ biobank-plugin/public/partials/components/quiz.php
@@ -3,6 +3,7 @@
 	<div class='question'>
 		<div class='row mx-1'><?= $question->question ?></div>
 		<div class='row explanation'><?= $question->explanation ?></div>
+		<input type='hidden' value='<?= $question->answer ?>'>
 		<div class='row mx-3 mb-2'>
 			<div class='col-md-6 text-center'>
 				<input id="biobank-question-<?= $i ?>-yes" class='radio' value='yes' type='radio'>
