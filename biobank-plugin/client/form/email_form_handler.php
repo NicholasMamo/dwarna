0c6c499e40ed9b040b966dec33d80f1b8bcca4be
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-17 15:21:22 +0200

    Create a new email form handler

---
+++ biobank-plugin/client/form/email_form_handler.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * The file that handles email form submissions.
+ * The functions in this class are used to help process consent form submissions.
+ * They also handle data retrieval.
+ *
+ * @link       https://github.com/nmamo
+ * @since      1.0.0
+ *
+ * @package    Biobank
+ * @subpackage Biobank/client/forms
+ */
+
+namespace client\form;
+
+require_once(plugin_dir_path(__FILE__) . "form_handler.php");
+
+/**
+ * The form handler class used to manage study inputs from forms.
+ *
+ * This class validates and sanitizes inputs from various forms.
+ *
+ * @since      1.0.0
+ * @package    Biobank
+ * @subpackage Biobank/client
+ * @author     Nicholas Mamo <nicholas.mamo@um.edu.mt>
+ */
+class EmailFormHandler extends FormHandler {
+
+	/**
+	 * Create a new email.
+	 *
+	 * @since	1.0.0
+	 * @access	public
+	 */
+	public function create_email() {
+	}
+
+}
