cf533c187956a01fe16cbf3ea9809ed08b043301
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-21 16:25:43 +0200

    Add tests to get all emails

--- rest/tests/test_email_management.py
+++ rest/tests/test_email_management.py
@@ -141,6 +141,70 @@ class EmailManagementTest(BiobankTestCase):
 		self.assertEqual(body, response_body['body'])
 
 	@BiobankTestCase.isolated_test
+	def test_get_all_emails(self):
+		"""
+		Test getting all emails.
+		The test is simple and looks only to retrieve the basic information, not the recipients.
+		"""
+
+		subject = 'Email %s'
+		body = "Body %s"
+		token = self._get_access_token(["create_email", "view_email"])["access_token"]
+
+		"""
+		Assert that initially there are no emails.
+		"""
+		response = self.send_request("GET", "email", {}, token)
+		self.assertEqual(response.status_code, 200)
+		response_body = response.json()['data']
+		self.assertEqual(0, len(response_body))
+
+		"""
+		Create the first email.
+		"""
+
+		response = self.send_request("POST", "email", {
+			"subject": subject % 1,
+			"body": body % 1
+		}, token)
+		self.assertEqual(response.status_code, 200)
+		response_body = response.json()
+		id = response_body['id']
+
+		"""
+		Assert that the data is correct.
+		"""
+		response = self.send_request("GET", "email", {}, token)
+		self.assertEqual(response.status_code, 200)
+		response_body = response.json()['data']
+		self.assertEqual(1, len(response_body))
+		self.assertEqual(id, response_body[0]['id'])
+		self.assertEqual(subject % 1, response_body[0]['subject'])
+		self.assertEqual(body % 1, response_body[0]['body'])
+
+		"""
+		Create the second email.
+		"""
+
+		response = self.send_request("POST", "email", {
+			"subject": subject % 2,
+			"body": body % 2
+		}, token)
+		self.assertEqual(response.status_code, 200)
+		response_body = response.json()
+		id = response_body['id']
+
+		"""
+		Assert that the data is correct.
+		"""
+		response = self.send_request("GET", "email", {}, token)
+		self.assertEqual(response.status_code, 200)
+		response_body = response.json()['data']
+		self.assertEqual(2, len(response_body))
+		self.assertEqual(subject % 2, response_body[1]['subject'])
+		self.assertEqual(body % 2, response_body[1]['body'])
+
+	@BiobankTestCase.isolated_test
 	def test_get_email_recipients(self):
 		"""
 		Test getting an email and its recipients.
