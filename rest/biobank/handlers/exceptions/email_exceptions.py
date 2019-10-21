78aa2759cdd764db91579d68ae28f8ae475e39db
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-21 17:05:59 +0200

    Add exceptions for when the recipient group is unknown or unsupported

--- rest/biobank/handlers/exceptions/email_exceptions.py
+++ rest/biobank/handlers/exceptions/email_exceptions.py
@@ -9,3 +9,19 @@ class EmailDoesNotExistException(Exception):
 
 	def __init__(self, id, message="The email with ID %d does not exist"):
 		super(EmailDoesNotExistException, self).__init__(message % id)
+
+class UnknownRecipientGroupException(Exception):
+	"""
+	An exception that indicates that the given recipient group is not known.
+	"""
+
+	def __init__(self, recipient_group, message="Recipient group '%s' is unknown"):
+		super(UnknownRecipientGroupException, self).__init__(message % recipient_group)
+
+class UnsupportedRecipientGroupException(Exception):
+	"""
+	An exception that indicates that the given recipient group is not supported.
+	"""
+
+	def __init__(self, recipient_group, message="Recipient group '%s' is not supported"):
+		super(UnsupportedRecipientGroupException, self).__init__(message % recipient_group)
