a5d40db7fb67021abb49670c93ba669d4f7c5f1c
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-21 12:34:12 +0200

    Add initial email REST API test class

---
+++ rest/tests/test_email_management.py
@@ -0,0 +1,25 @@
+"""
+Test the user management functionality in the backend.
+"""
+
+import json
+import os
+import sys
+
+path = sys.path[0]
+path = os.path.join(path, "../")
+if path not in sys.path:
+	sys.path.insert(1, path)
+
+import main
+
+from .environment import *
+
+from .test import BiobankTestCase
+
+class EmailManagementTest(BiobankTestCase):
+	"""
+	Test the backend's email management functionality.
+	"""
+
+	pass
