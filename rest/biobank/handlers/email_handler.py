754af42b96fd9f8918c70e4bb6a9656060753291
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-22 13:17:44 +0200

    Update documentation to conform with Sphinx

--- rest/biobank/handlers/email_handler.py
+++ rest/biobank/handlers/email_handler.py
@@ -24,17 +24,19 @@ class EmailHandler(PostgreSQLRouteHandler):
 		:param body: The email's body.
 		:type body: str
 		:param recipients: The email's recipients.
-			If `None` is given, the recipients default to an empty list.
-			Otherwise, the list is considered as a list of emails.
+						   If `None` is given, the recipients default to an empty list.
+						   Otherwise, the list is considered as a list of emails.
+
 		:type recipients: None or list
 		:param recipient_group: A recipient group that should receive the email.
-			The group is represented as a string, or nothing at all.
-			The conversion from the group to the actual recipients is handled by the function.
-			Accepted strings:
-				- 'None' - no recipient should be added
-				- 'Subscribed' - only subscribed users should receive the email
-				- 'All' - everyone, including unsubscribed users, should receive the email.
-				  *This should be used sparingly and only when absolutely needed to respect user decisions.*
+								The group is represented as a string, or nothing at all.
+								The conversion from the group to the actual recipients is handled by the function.
+								Accepted strings:
+								- 'None' - no recipient should be added
+								- 'Subscribed' - only subscribed users should receive the email
+								- 'All' - everyone, including unsubscribed users, should receive the email.
+								*This should be used sparingly and only when absolutely needed to respect user decisions.*
+
 		:type recipient_group: None or str
 
 		:return: A response with any errors that may arise.
