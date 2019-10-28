ca4fa2c44fba6034d229ed6c72028858863b045d
Nicholas Mamo <nicholasmamo@gmail.com>
2019-10-28 13:23:52 +0100

    Fix buttons having no effect by removing URL string beautification in the admin-side

--- biobank-plugin/includes/class-biobank.php
+++ biobank-plugin/includes/class-biobank.php
@@ -163,7 +163,7 @@ class Biobank {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
-		$this->loader->add_action( 'admin_init', $plugin_admin, 'redirect' ); // to prettify URLs - places GET parameters in session
+		// $this->loader->add_action( 'admin_init', $plugin_admin, 'redirect' ); // to prettify URLs - places GET parameters in session
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
 		$this->loader->add_action( 'admin_init', $plugin_admin, 'options_update' );
 
