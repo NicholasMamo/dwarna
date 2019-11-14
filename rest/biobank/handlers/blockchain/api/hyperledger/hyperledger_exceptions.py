c689ea228b30dc7eaf435c6d896140c5381fead1
Nicholas Mamo <nicholasmamo@gmail.com>
2019-11-14 14:13:37 +0100

    Create a new Hyperledger exception for unauthorized access

--- rest/biobank/handlers/blockchain/api/hyperledger/hyperledger_exceptions.py
+++ rest/biobank/handlers/blockchain/api/hyperledger/hyperledger_exceptions.py
@@ -9,3 +9,11 @@ class StudyAssetExistsException(Exception):
 
 	def __init__(self, id, message=f"Study %s already exists"):
 		super(StudyAssetExistsException, self).__init__(message % id)
+
+class UnauthorizedDataAccessException(Exception):
+	"""
+	An exception that indicates that a request could not be completed because the access was unauthorized.
+	"""
+
+	def __init__(self, message=f"Unauthorized access"):
+		super(UnauthorizedDataAccessException, self).__init__(message)
