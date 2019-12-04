93803d25c1e0ee63b372f1b30b08aff7f1dae3be
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-04 10:49:44 +0100

    Add a new email configuration file

---
+++ rest/config/email.example.py
@@ -0,0 +1,47 @@
+smtp_host = "smtp.gmail.com"
+"""
+:var smtp_host: The SMTP host.
+:vartype smtp_host: str
+"""
+
+smtp_port = 587
+"""
+:var smtp_port: The SMTP port.
+:vartype smtp_port: int
+"""
+
+smtp_secure = 'tls'
+"""
+:var smtp_tls: The SMTP security protocol.
+:vartype smtp_tls: str
+"""
+
+smtp_auth = True
+"""
+:var smtp_auth: A boolean indicating whether the SMTP should be authenticated or not.
+:vartype smtp_auth: bool
+"""
+
+smtp_user = 'joeborg@gmail.com'
+"""
+:var smtp_user: The authentication username.
+:vartype smtp_user: str
+"""
+
+smtp_pass = 'xxxxxxxxxxxxxxxx'
+"""
+:var smtp_pass: The authentication password.
+:vartype smtp_pass: str
+"""
+
+smtp_from = 'joeborg@gmail.com'
+"""
+:var smtp_from: The email address that is shown as sending emails.
+:vartype smtp_from: str
+"""
+
+smtp_name = 'Joe Borg'
+"""
+:var smtp_name: The display name that is shown as sending emails.
+:vartype smtp_name: str
+"""
