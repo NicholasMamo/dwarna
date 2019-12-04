da52fdbed4b393bcad14cca16d7b14e122231d9f
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-04 11:32:51 +0100

    Update the OAuth example configuration file with database constraints

--- rest/config/oauth.example.py
+++ rest/config/oauth.example.py
@@ -28,29 +28,31 @@ token_expiry = 120
 :vartype token_expiry: int
 """
 
-client_id = 'abc'
+client_id = '2d7db5ed5ca043c68cc9ff01f405a930'
 """
 :var client_id: The client ID for the OAuth 2.0 Client Credentials workflow.
-				The client ID can be a `UUID <https://www.ietf.org/rfc/rfc4122.txt>`_.
+				The client ID can be based on a `UUID <https://www.ietf.org/rfc/rfc4122.txt>`_.
+				By default, the database expects a 32-character string. Therefore the dashes are removed.
 				To generate a UUID:
 
                 .. code-block:: python
 
 				   import uuid
-				   str(uuid.uuid4())
+				   str(uuid.uuid4()).replace('-', '')
 
 :vartype client_id: str
 """
 
-client_secret = 'xyz'
+client_secret = 'Y41uqXoj9FMfK4D3cTmTxEZr0oB7Eisi'
 """
 :var client_secret: The client secret for the OAuth 2.0 Client Credentials workflow.
-					The string length can be arbitrary. To generate a secret:
+					The string length can be up to 32 characters by default.
+					To generate a secret:
 
 					.. code-block:: python
 
 					   import random
 					   import string
-					   ''.join(random.choice(string.ascii_letters + string.digits) for i in range(0, 64))
+					   ''.join(random.choice(string.ascii_letters + string.digits) for i in range(0, 32))
 :vartype client_secret: str
 """
