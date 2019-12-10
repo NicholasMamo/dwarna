d149267b38f8f5beca268dbdeb6939ad48569ad0
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-10 14:50:26 +0100

    Add functionality to take a backup of the fabric data

--- backup.sh
+++ backup.sh
@@ -2,3 +2,13 @@
 
 backup=$( date +%Y%m%d )
 mkdir -p backup/$backup
+
+backup_fabric() {
+	mkdir -p backup/$1/fabric/dwarna-blockchain
+	cp fabric/dwarna-blockchain/admin@dwarna-blockchain.card backup/$1/fabric/dwarna-blockchain
+
+	mkdir -p backup/$1/fabric/fabric-scripts/hlfv12/composer
+	cp -r fabric/fabric-scripts/hlfv12/composer/backup_* backup/$1/fabric/fabric-scripts/hlfv12/composer
+}
+
+backup_fabric $backup
