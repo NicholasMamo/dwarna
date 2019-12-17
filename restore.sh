394ee247ee4e4086aa03866880362f5514023d40
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-17 13:41:12 +0100

    Integrate the path parameter with the restore script

--- restore.sh
+++ restore.sh
@@ -4,12 +4,12 @@ source variables.sh
 
 function usage() {
 	echo -e "Usage: sh $0 -p path";
-	echo -e "    -p path    The path of the backup to restore, for example 'backup/20191217";
+	echo -e "       -p path    The path of the backup to restore, for example 'backup/20191217'";
 }
 
 if getopts "p:" o
 then
-	echo ""
+	path=${OPTARG}
 else
 	usage
 fi
