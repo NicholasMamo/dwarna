30982ca9e3707ccf6fc4bcbc98af159c8fc038f0
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-17 11:58:10 +0100

    Add the usage instructions to the restore script

--- restore.sh
+++ restore.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 source variables.sh
+
+function usage() {
+	echo -e "Usage: sh $0 -p path";
+	echo -e "    -p path    The path of the backup to restore, for example 'backup/20191217";
+}
+
+if getopts "p:" o
+then
+	echo ""
+else
+	usage
+fi
