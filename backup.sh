0d3bb2e3cce75d3c932653f9105de13a1c604776
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-17 11:47:36 +0100

    Add output to the backup script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source variables.sh
+
 backup=$( date +%Y%m%d )
 mkdir -p backup/$backup
 
@@ -9,6 +11,7 @@ cd "$parent_path"
 
 # The Hyperledger Fabric backup copies the admin card and the actual data.
 backup_fabric() {
+	echo -e "${HIGHLIGHT}Backing up Hyperledger Fabric files${DEFAULT}"
 	mkdir -p backup/$1/fabric/dwarna-blockchain
 	cp fabric/dwarna-blockchain/admin@dwarna-blockchain.card backup/$1/fabric/dwarna-blockchain
 
@@ -18,18 +21,21 @@ backup_fabric() {
 
 # The REST API backup copies the configuration, including the encryption keys.
 backup_rest() {
+	echo -e "${HIGHLIGHT}Backing up REST API files${DEFAULT}"
 	mkdir -p backup/$1/rest/config
 	cp rest/config/*.py backup/$1/rest/config
 }
 
 # The WordPress plugin backup copies the configuration, including the encryption key.
 backup_plugin() {
+	echo -e "${HIGHLIGHT}Backing up WordPress plugin files${DEFAULT}"
 	mkdir -p backup/$1/biobank-plugin/includes
 	cp biobank-plugin/includes/globals.php backup/$1/biobank-plugin/includes/globals.php
 }
 
 # The PostgreSQL backup creates a CSV file of each Dwarna table.
 backup_postgresql() {
+	echo -e "${HIGHLIGHT}Backing up PostgreSQL database${DEFAULT}"
 	mkdir -p backup/$1/postgresql/
 	chown postgres backup/$1/postgresql/
 
@@ -42,6 +48,7 @@ backup_postgresql() {
 
 # The WordPress backup creates a CSV file of each WordPress table.
 backup_wordpress() {
+	echo -e "${HIGHLIGHT}Backing up WordPress database${DEFAULT}"
 	mkdir -p backup/$1/wordpress/
 	mysqldump -u root -p wordpress > backup/$1/wordpress/wordpress.dump
 }
