7919656f4ced541fad240d3bd252e8c6cc8f662c
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-17 14:24:00 +0100

    Add WordPress database restoring functionality to the restore script

--- restore.sh
+++ restore.sh
@@ -7,9 +7,20 @@ function usage() {
 	echo -e "       -p path    The path of the backup to restore, for example 'backup/20191217'";
 }
 
+# Restore the WordPress database.
+function restore_wordpress() {
+	echo -e "${HIGHLIGHT}Restoring WordPress database${DEFAULT}"
+	mkdir -p backup/$1/wordpress/
+	read -p 'Enter database [wordpress]: ' database
+	database=${database:-wordpress}
+	read -p 'Enter username: ' username
+	mysql -u $username -p $database < $1/wordpress/wordpress.sql
+}
+
 if getopts "p:" o
 then
 	path=${OPTARG}
+	restore_wordpress $path
 else
 	usage
 fi
