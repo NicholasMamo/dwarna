a544c11da1dded7c0e3dcf9140c557ad04551a47
Nicholas Mamo <nicholasmamo@gmail.com>
2019-12-17 15:00:21 +0100

    Add functionality to restore the PostgreSQL database

--- restore.sh
+++ restore.sh
@@ -7,6 +7,21 @@ function usage() {
 	echo -e "       -p path    The path of the backup to restore, for example 'backup/20191217'";
 }
 
+# Restore the PostgreSQL database.
+restore_postgresql() {
+	echo -e "${HIGHLIGHT}Backing up PostgreSQL database${DEFAULT}"
+	read -p 'Enter database [biobank]: ' database
+	database=${database:-biobank}
+
+	parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+	tables=( users researchers participants participant_identities participant_subscriptions biobankers studies studies_researchers emails email_recipients )
+	for table in "${tables[@]}"
+	do
+		su -c "psql -U postgres -d $database -c \"COPY $table FROM '$parent_path/$1/postgresql/$table.csv' DELIMITER ',' CSV HEADER;\"" postgres
+	done
+}
+
 # Restore the WordPress database.
 function restore_wordpress() {
 	echo -e "${HIGHLIGHT}Restoring WordPress database${DEFAULT}"
@@ -20,6 +35,7 @@ function restore_wordpress() {
 if getopts "p:" o
 then
 	path=${OPTARG}
+	restore_postgresql $path
 	restore_wordpress $path
 else
 	usage
