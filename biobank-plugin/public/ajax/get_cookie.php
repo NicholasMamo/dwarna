c99fbc64632c8cd478d23ac6b5923eef722f2eaa
NicholasMamo <nicholasmamo@gmail.com>
2020-01-20 13:55:56 +0100

    Create a new AJAX script to get the access token cookie

---
+++ biobank-plugin/public/ajax/get_cookie.php
@@ -0,0 +1,11 @@
+<?php
+
+include(plugin_dir_path(__FILE__) . "../../includes/globals.php");
+
+if (isset($_GET["redirect"])) {
+	$redirect = $_GET["redirect"];
+	$cookie = $_COOKIE[$blockchain_access_token];
+	header("Location: $redirect?$blockchain_access_token=$cookie");
+}
+
+?>
