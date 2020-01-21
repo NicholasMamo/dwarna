7d4551024ccbc0ad7402d951ce474ddcf6793471
Nicholas Mamo <nicholasmamo@gmail.com>
2020-01-21 14:26:00 +0100

    Fix username and password not being inserted when creating users

--- biobank-plugin/partials/emails/biobank-email-create-researcher.php
+++ biobank-plugin/partials/emails/biobank-email-create-researcher.php
@@ -2,8 +2,8 @@
 	<body>
 		<?php include(plugin_dir_path(__FILE__) . "/biobank-email-header.php"); ?>
 		<p>Your new Dwarna account is ready to be used. Keep your password safe and use it to log in to Dwarna.</p>
-		<p>Username: {$input['username']}</p>
-		<p>Password: {$input['password']}</p>
+		<p>Username: <?= $input['username'] ?></p>
+		<p>Password: <?= $input['password'] ?></p>
 		<?php include(plugin_dir_path(__FILE__) . "/biobank-email-footer.php"); ?>
 	</body>
 </html>
