a64aa0bafa8b940ed2c7f4e01dfd2daca37ade19
Nicholas Mamo <nicholasmamo@gmail.com>
2020-01-23 16:29:17 +0100

    Rename the options form

--- biobank-plugin/partials/admin/biobank-admin-settings.php
+++ biobank-plugin/partials/admin/biobank-admin-settings.php
@@ -29,11 +29,10 @@ require_once(plugin_dir_path(__FILE__) . "../ui/notices.php");
 		}
 	?>
 
-    <form class="<?= $this->plugin_name ?>-form" id="connection_form" method="post" name="connection_form" action="options.php">
+	<h2 class="title">REST API</h2>
+    <form class="<?= $this->plugin_name ?>-form" id="rest-form" method="post" name="rest-form" action="options.php">
 		<?php settings_fields($this->plugin_name); ?>
 		<?php do_settings_sections($this->plugin_name); ?>
-
-		<h2 class="title">REST API</h2>
 
 		<table class="form-table">
 
