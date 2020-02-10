35d88d5d6279097e1acd363b563b00d8fac14389
NicholasMamo <nicholasmamo@gmail.com>
2020-02-10 14:56:34 +0100

    Create a new directory for tools

--- tools/backup.sh
+++ tools/backup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Go to the script directory.
+# Go to the script's parent directory.
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
+cd "$parent_path/.."
 
 source variables.sh
 
