e915561c6814678266d7219c4ab0a34910d2fd29
Nicholas Mamo <nicholasmamo@gmail.com>
2020-02-11 16:10:36 +0100

    Create a new configuration to erase research partners from the REST API

---
+++ rest/config/example/erasure.py
@@ -0,0 +1,14 @@
+script = ""
+"""
+:var script: The path to the erasure script.
+			 This script is called after deleting research partners.
+			 It removes the research partners from the backups.
+			 If it is empty, backup erasure is not performed.
+:vartype script: str
+"""
+
+backups = ""
+"""
+:var backups: The path to the backup directory.
+:vartype backups: str
+"""
