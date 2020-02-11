9ef37e080561ad81e9d758dc70b1d1f96e1fbf65
Nicholas Mamo <nicholasmamo@gmail.com>
2020-02-11 14:15:53 +0100

    Create the usage function

--- tools/erase.sh
+++ tools/erase.sh
@@ -5,3 +5,10 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path/.."
 
 source variables.sh
+
+function usage() {
+	echo -e "Usage: sh $0 -p path [-h] [pseudonym]...";
+	echo -e "       -p path    The path of the backups folder, for example 'backup/20191217'";
+	echo -e "       The last arguments are taken to be pseudonyms";
+}
+
