b5cafca7090a812c57e63f0cb7bbda65ceabb53a
Nicholas Mamo <nicholasmamo@gmail.com>
2020-02-11 12:29:06 +0100

    Add the command line arguments to the WP erasure script

--- tools/erase_wordpress.py
+++ tools/erase_wordpress.py
@@ -4,4 +4,29 @@
 A function to go through the WordPress data file and erase a list of research partners.
 """
 
-pass
+import argparse
+import os
+import sys
+
+def setup_args():
+	"""
+	Set up and get the list of command-line arguments.
+
+	Accepted arguments:
+		- -e --erase	A list of research partners to erase.
+		- -p --path		The path to the backup directory.
+
+	:return: The command-line arguments.
+	:rtype: list
+	"""
+
+	parser = argparse.ArgumentParser(description="Erase research partners from PostgreSQL backups.")
+	parser.add_argument("-p", "--path", nargs=1, required=True,
+						help="<Required> The path to the backup directory.")
+	parser.add_argument("-e", "--erase", nargs="+", required=True,
+						help="<Required> A list of research partners to erase.")
+	args = parser.parse_args()
+	return args
+
+if __name__ == "__main__":
+	args = setup_args()
