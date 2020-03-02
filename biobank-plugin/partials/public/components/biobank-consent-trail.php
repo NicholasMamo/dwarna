1d2c693069af696029ced4c57b292b34af476ad9
NicholasMamo <nicholasmamo@gmail.com>
2020-03-02 13:55:04 +0100

    Add a wrapper for the table

--- biobank-plugin/partials/public/components/biobank-consent-trail.php
+++ biobank-plugin/partials/public/components/biobank-consent-trail.php
@@ -1,20 +1,22 @@
 <div id='<?= $this->plugin_name ?>-consent-trail'>
 	<h2>Consent trail</h2>
 	<div class='<?= $this->plugin_name ?>-wrapper'>
-		<?php foreach ($timeline as $timestamp => $changes) { ?>
-		<div class='<?= $this->plugin_name ?>-node'>
-			<div class='<?= $this->plugin_name ?>-datetime'>
-				<div class='<?= $this->plugin_name ?>-date'><?= DateTime::createFromFormat("U", $timestamp)->format("j M Y") ?></div>
-				<div class='<?= $this->plugin_name ?>-time'><?= DateTime::createFromFormat("U", $timestamp)->format("H:i") ?></div>
+		<div class='<?= $this->plugin_name ?>-table'>
+			<?php foreach ($timeline as $timestamp => $changes) { ?>
+			<div class='<?= $this->plugin_name ?>-node'>
+				<div class='<?= $this->plugin_name ?>-datetime'>
+					<div class='<?= $this->plugin_name ?>-date'><?= DateTime::createFromFormat("U", $timestamp)->format("j M Y") ?></div>
+					<div class='<?= $this->plugin_name ?>-time'><?= DateTime::createFromFormat("U", $timestamp)->format("H:i") ?></div>
+				</div>
+				<?php foreach ((array) $changes as $study_id => $consent) { ?>
+						<?php if ($study_id == $study->study->study_id) { ?>
+						<div class='<?= $this->plugin_name ?>-consent-change'>
+							<span>You <?= $consent == 1 ? "gave" : "withdrew" ?> consent</span>
+						</div>
+						<?php } ?>
+				<?php } ?>
 			</div>
-			<?php foreach ((array) $changes as $study_id => $consent) { ?>
-					<?php if ($study_id == $study->study->study_id) { ?>
-					<div class='<?= $this->plugin_name ?>-consent-change'>
-						<span>You <?= $consent == 1 ? "gave" : "withdrew" ?> consent</span>
-					</div>
-					<?php } ?>
 			<?php } ?>
 		</div>
-		<?php } ?>
 	</div>
 </div>
