25b73ab0f6e4eba9a5d47ceec3e8041f390f947a
Nicholas Mamo <nicholasmamo@gmail.com>
2020-03-10 15:17:52 +0100

    Create a new backup script that sends the backup by email

---
+++ cron/backup.sh
@@ -0,0 +1,12 @@
+./tools/backup.sh --rest --plugin --postgresql --wordpress -o /mnt/data/backups -z
+
+# Go to the script's parent directory.
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+source $parent_path/../variables.sh
+
+if [ -f $parent_path/email.conf ]; then
+	. $parent_path/email.conf
+	file=$( date +%Y%m%d )
+	fdate=$( date +%d/%m/%Y )
+	echo "Automated email: backup taken on $fdate attached" | mailx -s '[Automated] Backup $fdate' $email -a "From: $email" -A backup/$file.tar.gz
+fi
