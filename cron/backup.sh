ba7589d331d19e46f205763121d70aa489ab20b8
Nicholas Mamo <nicholasmamo@gmail.com>
2020-03-10 15:19:49 +0100

    Remove needless variable importing

--- cron/backup.sh
+++ cron/backup.sh
@@ -2,7 +2,6 @@
 
 # Go to the script's parent directory.
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-source $parent_path/../variables.sh
 
 if [ -f $parent_path/email.conf ]; then
 	. $parent_path/email.conf
