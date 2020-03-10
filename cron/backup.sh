5a215df446883817f7461552577123c9625932ea
Nicholas Mamo <nicholasmamo@gmail.com>
2020-03-10 15:36:20 +0100

    Update the data folders

--- cron/backup.sh
+++ cron/backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./tools/backup.sh --rest --plugin --postgresql --wordpress -o /mnt/data/backups -z
+./tools/backup.sh --rest --plugin --postgresql --wordpress -o /mnt/DATA/backups -z
 
 # Go to the script's parent directory.
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
