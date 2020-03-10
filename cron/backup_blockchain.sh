2cf01506fd99d6d54b53cc7f92e48c9c04786bf9
Nicholas Mamo <nicholasmamo@gmail.com>
2020-03-10 15:35:17 +0100

    Create new blockchain backup script

---
+++ cron/backup_blockchain.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+./tools/backup.sh ./tools/backup.sh -o /mnt/data/backups -z
+
+# Go to the script's parent directory.
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+if [ -f $parent_path/email.conf ]; then
+	. $parent_path/email.conf
+	file=$( date +%Y%m%d )
+	fdate=$( date +%d/%m/%Y )
+	echo "" | mailx -s "[Automated] Blockchain backup $fdate" $to -a "From: $to" -A backup/$file.tar.gz
+fi
